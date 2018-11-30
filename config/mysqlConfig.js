deeca14e08c6cc9cab8711fd05959c5077ce7248
mazhaoyou <953866349@qq.com>
2018-11-30 13:59:50 +0800

    初始提交

---
+++ config/mysqlConfig.js
@@ -0,0 +1,11 @@
+module.exports = {
+    connection: {
+        host: "188.131.174.180",
+        port: 3306,
+        user: "root",
+        password: "mzy123",
+        database: "blog",
+        multipleStatements: true //允许执行多条语句
+
+    }
+}
